30548551add1306d575f808b03e8eba529757236
Nasir Hussain <aghanhussain@gmail.com>
2022-10-04 02:39:49 +0500

    Union type of data is declared and initialized

---
+++ uniontypes.js
@@ -0,0 +1,9 @@
+// in union type of data for an object or a
+// variable we define multiple type together for a variable using |
+var myName = "Nasir Hussain";
+// valid
+myName = 500;
+myName = "Umer";
+myName = true;
+// any allow any type of value 
+//  union types allow only mentioned types
---
+++ uniontypes.ts
@@ -0,0 +1,12 @@
+// in union type of data for an object or a
+// variable we define multiple type together for a variable using |
+
+let myName:string|number|boolean = "Nasir Hussain"
+
+// valid
+myName = 500
+myName = "Umer"
+myName = true
+
+// any allow any type of value 
+//  union types allow only mentioned types
